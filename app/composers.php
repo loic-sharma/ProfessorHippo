a5c0a6d157379c6631f9f97e9997594798f41613
Loic Sharma <sharma.loic@gmail.com>
2013-02-02 04:17:10 -0500

    Fixed bug with filters.

--- app/composers.php
+++ app/composers.php
@@ -4,13 +4,6 @@ View::composer('layout', function($event)
 {
 	$event->view->navigation = Menu::get('main');
 
-	// Suppress a nasty error message if the content
-	// isn't set.
-	if( ! isset($event->view->content))
-	{
-		$event->view->content = '';
-	}
-
 	$event->view->sidebar = 'Sidebar';
 
 	$event->view->loadtime = round(microtime(true)-LARAVEL_START, 4);
